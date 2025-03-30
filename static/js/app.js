edf1c9b41a206bec98475973d13a374095cabdac
Iveth2703 <160411482+Torres-luziveth@users.noreply.github.com>
2025-03-30 15:57:31 -0500

    folder commits

---
+++ static/js/app.js
@@ -0,0 +1,11 @@
+
+function toggleSlider() {
+    let sliderBtn = document.getElementById("sliderBtn");
+    sliderBtn.classList.toggle("active");
+
+    if (sliderBtn.classList.contains("active")) {
+        setTimeout(() => {
+            document.querySelector(".singUp-form").submit();
+        }, 300);
+    }
+}
